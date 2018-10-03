cab192398f72890e3a3a3d14184708fa83deb139
Joey Öhman <joeyoh@kth.se>
2018-10-03 17:21:39 +0200

    add product sites and product reviews

--- model/Review.php
+++ model/Review.php
@@ -3,7 +3,7 @@ class Review {
   public $author, $product, $creation, $rating, $comment;
 
   public function __construct($row) {
-    $this->author = $row['review_auhtor'];
+    $this->author = $row['review_author'];
     $this->product = $row['review_product'];
     $this->creation = $row['review_creation'];
     $this->rating = $row['review_rating'];
