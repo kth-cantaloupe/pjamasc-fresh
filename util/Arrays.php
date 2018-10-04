0c46b092c37174c9fc4c1075a5f6818c956e64f6
sebastian-porling <sebastian.porling@gmail.com>
2018-10-04 17:14:19 +0200

    Contact, index done. Layout almost done

--- util/Arrays.php
+++ util/Arrays.php
@@ -10,7 +10,7 @@ class Arrays {
 	public static function areset($array, $keys) {
 		foreach ($keys as $key)
 			if (!isset($array[$key]))
-				return false;
+                return false;
 
 		return true;
 	}
